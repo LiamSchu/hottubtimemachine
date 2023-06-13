753f15e71bc1086ab1daf1cb644c4f9b9520b029
Liam Schubert <liam@schubert.dev>
2023-06-13 12:36:16 -0600

    [add] debounce filter, temp check delay

--- HotTubTimeMachine.ino
+++ HotTubTimeMachine.ino
@@ -10,7 +10,7 @@
 #define HYSTERESIS 0.25
 
 // how many samples to take and average, more takes longer but is more 'smooth'
-#define NUMSAMPLES 50
+#define NUMSAMPLES 100
 // Set the desired hot tub temperature
 float desiredTemp = 101;
 
@@ -28,17 +28,21 @@ const uint8_t RELAY_HEATER_PIN = 8, // the number of the Relay 1 Digital I/O pin
               WATER_TEMP_PIN = 14;     // on arduino uno, pin 14 is A0 analog pin
 //              HEATER_TEMP_PIN = 15;  // pin A1
            
-// Delay between checking or changing states of heater/pump/etc
-const long heaterCheckDelay = 10000;  // 5 minutes: interval at which to potentially change whether the heater is on or off (milliseconds)
-
 // Start with the relays open (heater off)
 int relayHeaterState = LOW;
 //    relay2State = LOW,
 //    relay3State = LOW,
 //    relay4State = LOW;
 
+// Variables to slow down the checks whether for how frequently the heater can be changed (avoid over-cycling when adjusting temperature), or how quickly to re-check the button (helps debounce - prevent multiple perceived button clicks for a single click)
 // Generally, you should use "unsigned long" for variables that hold time as the value will quickly become too large for an int to store
 unsigned long t0HeaterLoop = 0;  // will store last time the heater check was run
+const long heaterCheckDelay = 10000;  // 10 seconds: interval at which to potentially change whether the heater is on or off (milliseconds)
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+const long debounceDelay = 100;    // the debounce time; increase if the output flickers
+unsigned long lastTempCheck = 0;  // the last time the output pin was toggled
+const long tempCheckDelay = 1000;    // the debounce time; increase if the output flickers
+
 float waterTemp = 50;
 
 const int buttonTempUp = 9;  // button pin to increase temperature
@@ -49,10 +53,6 @@ float ChangeTempByDegrees = 0.5; // Press button to change temperature by this d
 const float MaxSetPointTemp = 104;
 const float MinSetPointTemp = 95;
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup(void) 
 {
@@ -67,10 +67,10 @@ void setup(void)
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Water Temp: ");
   lcd.setCursor(0, 1);
-  lcd.print("Heater is: ");
+  lcd.print("Heat to:   ");
+  lcd.print(desiredTemp);
+  // Print a message to the LCD.
   pinMode(buttonTempUp, INPUT);
   pinMode(buttonTempDn, INPUT);
 }
@@ -79,31 +79,33 @@ void loop(void)
 {
   waterTemp = RunHeaterCheck();
   Inc_Dec_Temp_Setpoint();
-//  randomTempCheck();
-//  delay(1000);
+  randomTempCheck();
 }
 
 void randomTempCheck()
 {
-  waterTemp = ReadThermistor(WATER_TEMP_PIN, SERIES_RESISTOR_WATER_TEMP, THERMISTOR_NOMINAL_WATER_TEMP);
-
-  Serial.print("Temperature "); 
-  Serial.print(waterTemp);
-  Serial.println(" *F");
-  Serial.print("Heater is: ");
-  if (relayHeaterState)
+  if (Is_It_Time(&lastTempCheck, &tempCheckDelay) == true)
   {
-    Serial.println("on");
-  } else {
-    Serial.println("off");
+    waterTemp = ReadThermistor(WATER_TEMP_PIN, SERIES_RESISTOR_WATER_TEMP, THERMISTOR_NOMINAL_WATER_TEMP);
+
+    Serial.print("Temperature "); 
+    Serial.print(waterTemp);
+    Serial.println(" *F");
+    Serial.print("Heater is: ");
+    if (relayHeaterState)
+    {
+      Serial.println("on");
+    } else {
+      Serial.println("off");
+    }
   }
 }
 
 float RunHeaterCheck()
 {
-  if (Is_It_Time(t0HeaterLoop, heaterCheckDelay) == true){
+  if (Is_It_Time(&t0HeaterLoop, &heaterCheckDelay) == true)
+  {
     waterTemp = ReadThermistor(WATER_TEMP_PIN, SERIES_RESISTOR_WATER_TEMP, THERMISTOR_NOMINAL_WATER_TEMP);
-
     // check the temperature and close the relay if it is too cold, open it if it's warm enough
     if (relayHeaterState==LOW)  // if heater is off, check if the water is cold
     {
@@ -142,16 +144,16 @@ float RunHeaterCheck()
   }
 }
 
-bool Is_It_Time(unsigned long timeLastReset, long interval)
+bool Is_It_Time(unsigned long *timeLastReset, long *interval)
 {
   // check to see if it's time to retest the heat (or other device); that is, if the difference
   // between the current time and last time the heater was adjusted is bigger than the defined interval
   unsigned long currentTimeMilliSec = millis();
-  const long elapsedTime = currentTimeMilliSec - timeLastReset;
-  if (elapsedTime >= interval) 
+  const long elapsedTime = currentTimeMilliSec - *timeLastReset;
+  if ((elapsedTime >= *interval) || (*timeLastReset == 0)) // run whenever the arduino is powered on/reset, an then at the appropriate intervals
   {
     // set the reset time to now because it is time to run the analysis
-    t0HeaterLoop = currentTimeMilliSec;
+    *timeLastReset = currentTimeMilliSec;
     return true;
   } else {
     return false;
@@ -179,7 +181,8 @@ float ReadThermistor(int pinNum, float seriesResistor, float thermistorNominal)
   temperatureF = 1 / ((log(thermistorImpedance / thermistorNominal)/BCOEFFICIENT) + (1.0 / (TEMPERATURENOMINAL + 273.15))); // Invert (1/B * ln(R/Ro) + (1/To))
   temperatureF = (temperatureF-273.15) * 9/5 + 32;                                                                 // convert temp C to temp F
   
-  lcd.setCursor(11, 0);
+  lcd.setCursor(0, 0);
+  lcd.print("Water Temp:");
   lcd.print(temperatureF);
 
   return temperatureF;
@@ -187,23 +190,26 @@ float ReadThermistor(int pinNum, float seriesResistor, float thermistorNominal)
 
 void Inc_Dec_Temp_Setpoint()
 {
-  if (buttonStateInc != digitalRead(buttonTempUp)) // Read I/O to see if button press action has changed (just pressed or just released)
+  if (Is_It_Time(&lastDebounceTime, &debounceDelay) == true)
   {
-    buttonStateInc = 1 - buttonStateInc; // True becomes False, False becomes True
-    if((desiredTemp < MaxSetPointTemp) && (buttonStateInc == true)) // Temp is below maximum and button has just been pressed
+    if (buttonStateInc != digitalRead(buttonTempUp)) // Read I/O to see if button press action has changed (just pressed or just released)
     {
-        desiredTemp += ChangeTempByDegrees; // Increase the setpoint temperature by defined amount
-        print_Temp_SP(); // show on LCD and send to serial (computer)
+      buttonStateInc = 1 - buttonStateInc; // True becomes False, False becomes True
+      if((desiredTemp < MaxSetPointTemp) && (buttonStateInc == true)) // Temp is below maximum and button has just been pressed
+      {
+          desiredTemp += ChangeTempByDegrees; // Increase the setpoint temperature by defined amount
+          print_Temp_SP(); // show on LCD and send to serial (computer)
+      }
     }
-  }
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonStateDec != digitalRead(buttonTempDn)) // Read I/O to see if button press action has changed (just pressed or just released)
-  {
-    buttonStateDec = 1 - buttonStateDec; // True becomes False, False becomes True
-    if((desiredTemp > MinSetPointTemp) && (buttonStateDec == true)) // Temp is above minimum and button has just been pressed
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonStateDec != digitalRead(buttonTempDn)) // Read I/O to see if button press action has changed (just pressed or just released)
     {
-      desiredTemp -= ChangeTempByDegrees; // Increase the setpoint temperature by defined amount
-      print_Temp_SP(); // show on LCD and send to serial (computer)
+      buttonStateDec = 1 - buttonStateDec; // True becomes False, False becomes True
+      if((desiredTemp > MinSetPointTemp) && (buttonStateDec == true)) // Temp is above minimum and button has just been pressed
+      {
+        desiredTemp -= ChangeTempByDegrees; // Increase the setpoint temperature by defined amount
+        print_Temp_SP(); // show on LCD and send to serial (computer)
+      }
     }
   }
 }
@@ -213,8 +219,6 @@ void print_Temp_SP()
   Serial.print("Temperature Set to: ");
   Serial.println(desiredTemp);
   lcd.setCursor(0, 1);
-  lcd.print("New Setpt:");
-  lcd.setCursor(11, 1);
-  lcd.println(desiredTemp);
-//  delay(50);
+  lcd.print("Heat to:   ");
+  lcd.print(desiredTemp);
 }
